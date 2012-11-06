61fe7851a24c007d4bc24150ad1bed5c54fee6b3
unknown <jproanio@USER.deltacomunicaciones.com>
2012-11-06 13:31:29 -0500

    slider

--- wp-content/themes/ppm/banner.php
+++ wp-content/themes/ppm/banner.php
@@ -1,1 +1,27 @@
-<div id="banner"></div>
+<div id="banner">
+
+<div class="coda-slider-wrapper">
+	<div class="coda-slider preload" id="coda-slider-1">
+
+
+	<!-- .LOOP -->
+
+	<?php query_posts("category_name=Slider&showposts=5"); 
+	
+	if (have_posts()) : while (have_posts()) : the_post(); ?>
+
+	
+		<div class="panel" style="background:url(<?php echo get_post_meta($post->ID,"slider",true)?>)">
+			<div class="panel-wrapper">
+				<h2><?php the_title();?></h2>
+				<?php the_content();?>
+			</div>
+		</div>
+	<?php endwhile;  else :  endif; ?>
+	<!-- .FIN LOOP -->
+
+
+		</div><!-- .coda-slider -->
+	</div>
+
+</div>
--- wp-content/themes/ppm/header.php
+++ wp-content/themes/ppm/header.php
@@ -1,28 +1,57 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-	<title><?php bloginfo('name'); ?></title>
-	<link rel="stylesheet" type="text/css" href="<?php bloginfo('stylesheet_url') ?>" />
-	<?php wp_head(); ?>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+<title><?php bloginfo('name'); ?></title>
+<link type="text/css" rel="stylesheet" href="<?php bloginfo('stylesheet_url')?>"/>
+<?php wp_head();?>
+	<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/jquery-1.3.2.min.js"></script>
+		<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/jquery.easing.1.3.js"></script>
+		<script type="text/javascript" src="<?php bloginfo('template_url')?>/js/jquery.coda-slider-2.0.js"></script>
+		 <script type="text/javascript">
+			$().ready(function() {
+				$('#coda-slider-1').codaSlider();
+			});
+		 </script>
 </head>
 
 <body>
-	<!--header-->
+
+	<!--HEADER-->
 	<div id="header">
+	
+	
+		<!--WRAP--->
 		<div class="wrap">
-			<!--Logo-->
+		
+		
+			<!--logo-->
 			<div id="logo">
-				<a href="<?php bloginfo('url')?>"><img src="<?php bloginfo('template_url')?>/images/logo.png" alt="logo" width="100"/></a>
+			
+			<a href="<?php bloginfo('url')?>"><img src="<?php bloginfo('template_url')?>/images/logo.png" alt="logo"/></a>
+			
 			</div>
-			<!--Fin Logo-->
-			<!--Menu-->
+			<!--fin logo-->
+			
+			<!--menu-->
 			<div id="menu">
+			
 				<ul>
 					<?php wp_list_pages( 'title_li=&depth=1' ); ?>
 				</ul>
+				
 			</div>
-			<!--Fin Menu-->
-			<?php include_once("banner.php"); ?>	<!--Slider-->
+			<!--fin menu-->
+			
+			
+			<!--banner-->
+			<?php include_once('banner.php'); ?>
+			<!--fin banner-->
+		
 		</div>
-	</div>
-	<!--Fin Header-->
+		<!--fin WRAP--->
+		
+		
+		
+	</div>	
+	<!-FIn header--->
